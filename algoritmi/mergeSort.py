a3bf927ec94eb058ec6ec9d8e574cb0cfe6fae5c
Seagull2004 <sea2004gull@gmail.com>
2025-03-23 12:23:01 +0100

    aggiunti contratti per mergesort

--- algoritmi/mergeSort.py
+++ algoritmi/mergeSort.py
@@ -1,9 +1,21 @@
 def mergeSort(A):
+    """
+    Ordina l'array A in ordine crescente usando il Merge Sort.
+    
+    Precondizione: A è una lista.
+    Postcondizione: A è ordinato in ordine non decrescente.
+    """
     if len(A) <= 0:
         return
     mergeSortAux(A, 0, len(A) - 1)
 
 def mergeSortAux(A, p, q):  
+    """
+    Funzione ausiliaria per il Merge Sort.
+    
+    Precondizione: 0 <= p <= q < len(A)
+    Postcondizione: A[p...q] è ordinato in ordine non decrescente.
+    """
 	if (p < q):
 		r = (p + q) // 2
 		mergeSortAux(A, p, r) 
@@ -11,6 +23,12 @@ def mergeSortAux(A, p, q):
 		merge(A, p, r, q)
 
 def merge(A, p, r, q):
+    """
+    Unisce due sottoarray ordinati di A: A[p...r] e A[r+1...q].
+    
+    Precondizione: A[p...r] e A[r+1...q] sono ordinati in ordine non decrescente.
+    Postcondizione: A[p...q] è ordinato in ordine non decrescente.
+    """
     B = [0] * (q - p + 1)
     k = 0 # indice di inserimento in B
     i = p # indice di lettura A[p...r]
