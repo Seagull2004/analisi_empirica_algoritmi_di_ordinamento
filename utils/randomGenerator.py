7b7f95005b606e2e758ccf4eb888aa28513c8d19
Frankrowbar <francescograz04@gmail.com>
2025-03-27 19:39:28 +0100

    aggiunto parametro per il valore minimo degli elementi nella generazione di array

--- utils/randomGenerator.py
+++ utils/randomGenerator.py
@@ -10,23 +10,24 @@ class Disposition(Enum):
 
 # resituisce un array di lunghezza len e con valori compresi in [0:max]
 # l'array è ordinato in senso crescente
-def riempiOrdinato(len: int, max: int):
+def riempiOrdinato(len: int, min: int, max: int):
     elements = []
     for _ in range(len):
-        bisect.insort(elements, random.randint(0, max))
+        bisect.insort(elements, random.randint(min, max))
     return elements
 
 # resituisce un array di lunghezza len e con valori compresi in [0:max]
 # l'array ha gli elementi disposti in maniera casuale
-def riempiCasualmente(len: int, max: int):
+def riempiCasualmente(len: int, min: int, max: int):
     elements = [0] * len
     for i in range(len):
-        elements[i] = random.randint(0, max)
+        elements[i] = random.randint(min, max)
     return elements
 
-def generaArray(len: int, max: int, disp: Disposition):
+def generaArray(len: int, min: int, max: int, disp: Disposition):
     """
     input: len lunghezza array
+           min minimo valore elementi
            max massimo valore elementi
            disp: 0 per avere un array ordinato in ordine crescente
                  1 per un array ordinato in ordine decrescente
@@ -36,9 +37,9 @@ def generaArray(len: int, max: int, disp: Disposition):
     assert(isinstance(disp, Disposition))
 
     if disp == Disposition.SORTED:
-        return riempiOrdinato(len, max)
+        return riempiOrdinato(len, min, max)
     if disp == Disposition.SORTED_REV:
-        arr = riempiOrdinato(len, max)
+        arr = riempiOrdinato(len, min, max)
         arr.reverse()
         return arr
-    return riempiCasualmente(len, max)   
+    return riempiCasualmente(len, min, max)   
