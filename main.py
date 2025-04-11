d6a85d40a6d4869f2070702966dbe2bdd4da6267
Marco Ungaro <Marco_Ungaro_mac@MacBook-Pro-di-Gianluca.local>
2025-04-11 15:29:48 +0200

    Aggiunto csv e file di prova e modificato il main

--- main.py
+++ main.py
@@ -4,23 +4,12 @@ import algoritmi.quicksort as QS
 import algoritmi.radixSort as RS
 import utils.randomGenerator as generator
 from utils.randomGenerator import Disposition
-import time
-import random
-import math
+import utils.measurement as measurement
+import utils.geometricRangeGenerator as generatorRange
+
+for i in generatorRange.generateGeometricRange(100, 100000, 100):
+    measurement.measureMeanTimeAlgo(CS.uniformedCountingSort, i, i)
 
 
 
 
-
-
-
-#for lunghezza in range(1000, 10000000, 10000):
-#    A = [random.randrange(6) for _ in range(lunghezza)]
-#    B = [0 for _ in range(len(A))]
-#
-#    start = time.perf_counter()
-#    while time.perf_counter() == start:
-#        pass
-#    CS.countingSort(A, B, 6)
-#    stop = time.perf_counter()
-#    print(lunghezza, ";", stop - start)
