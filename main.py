4b75a3d466751385c67b178ec6f4a0df120ec453
Seagull2004 <sea2004gull@gmail.com>
2025-04-27 09:14:42 +0200

    parametrizzato ulteriormente il main

--- main.py
+++ main.py
@@ -22,44 +22,85 @@ N_LOCK       = configuration["N_LOCK"]
 
 # Algoritmi e relative configurazioni
 algorithms = [
-    {"name": "Counting Sort", "algo": CS.uniformedCountingSort, "color": "black"},
-    {"name": "Quick Sort", "algo": QS.uniformedQuickSort, "color": "black"},
-    {"name": "Quick Sort 3-Way", "algo": QS3.uniformedQuickSort3Way, "color": "black"},
-    {"name": "Radix Sort", "algo": RS.uniformedRadixSort, "color": "black"},
+    {"name": "Counting Sort", "algo": CS.uniformedCountingSort, "color": "#26547c", "ordinate": [], "ascisse": []},
+    {"name": "Quick Sort", "algo": QS.uniformedQuickSort, "color": "#ef476f", "ordinate": [], "ascisse": []},
+    {"name": "Quick Sort 3-Way", "algo": QS3.uniformedQuickSort3Way, "color": "#ffd166", "ordinate": [], "ascisse": []},
+    {"name": "Radix Sort", "algo": RS.uniformedRadixSort, "color": "#06d6a0", "ordinate": [], "ascisse": []},
 ]
 
 
-# Creazione di sottotrame
-plt.figure(figsize=(12, 10))  # Aumenta la dimensione della figura per adattarsi a più sottotrame
-# ciclo sugli algoritmi che voglio cronometrare (countingSort, quicksort, ...)
-for idx, algo_config in enumerate(algorithms, start=1):
-    # log
-    print("Inizio a misurare",algo_config["name"])
-    # inizializzazione variabili
-    ascisse = [ 0 ] * NUM_CAMPIONI
-    ordinate = [ 0.0 ] * NUM_CAMPIONI
-    count = 0
-    # misurazione tempi
-    for n in generatorRange.generateGeometricRange(N_MIN, N_MAX, NUM_CAMPIONI):
-        ascisse[count] = n
-        ordinate[count] = measurement.measureMeanTimeAlgo(algo_config["algo"], n, m = M_LOCK) 
-        count += 1
-    # Creazione della sottotrama con una griglia 2x2
-    plt.subplot(2, 2, idx)
-    plt.loglog(ascisse, ordinate, marker='o', linestyle='', color=algo_config["color"], markersize=2)
-    plt.title(f'Tempi di esecuzione di {algo_config["name"]}', fontsize=16, fontweight='bold', pad=20)
-    plt.xlabel('Numero di elementi')
-    plt.ylabel('Tempo di esecuzione (s)')
-    # Calcolo della curva di adattamento in scala logaritmica
-    log_x = np.log10(ascisse)
-    log_y = np.log10(ordinate)
-    coeff = np.polyfit(log_x, log_y, 3)  # Fitting polinomiale di grado 3
-    fitted_curve = np.poly1d(coeff)
-    # Generazione dei valori della curva di adattamento
-    fitted_y = 10**fitted_curve(log_x)
-    # Aggiunta della curva di adattamento al grafico
-    plt.loglog(ascisse, fitted_y, linestyle='-', color='#ff000020', label='Curva di adattamento')
-    plt.legend()
-plt.tight_layout()
-plt.show()
+def misuraTempiSullaBaseDi(variabile: str, var_end: int, lock: int, var_start: int = 0):
+    """
+    Args:
+        variabile: 
+                può essere 'n' o 'm' sulla base della variabile che si vuole far variare (n, numero di elementi degli array o m, range numerico degli elementi dell'array)
+        var_start:
+                valore di partenza di 'n' o 'm'
+        var_end:
+                valore di termine di 'n' o 'm'
+        lock:
+                il valore a cui si sceglie di lockare l'altra variabile (e.g. se si fa variare n da 0 a 100, m può essere lockata a 100k)
+    """
+    # ciclo sugli algoritmi che voglio cronometrare (countingSort, quicksort, ...)
+    for idx, algo_config in enumerate(algorithms, start=1):
+        # log sul terminale
+        print("Inizio a misurare",algo_config["name"])
 
+        # inizializzazione variabili
+        ascisse = [ 0 ] * NUM_CAMPIONI
+        ordinate = [ 0.0 ] * NUM_CAMPIONI
+        measure_counter = 0
+
+        # misurazione tempi
+        for i in generatorRange.generateGeometricRange(var_start, var_end, NUM_CAMPIONI):
+            ascisse[measure_counter] = i
+            if variabile == 'n':
+                ordinate[measure_counter] = measurement.measureMeanTimeAlgo(algo_config["algo"], n = i, m = lock) 
+            else:
+                ordinate[measure_counter] = measurement.measureMeanTimeAlgo(algo_config["algo"], m = i, n = lock) 
+            measure_counter += 1
+
+        # salvataggio risultati nella struttura dati
+        algo_config["ordinate"] = ordinate
+        algo_config["ascisse"] = ascisse
+
+
+def stampaGraficoDeiValoriMisurati(x_label: str, log_scale: bool = False) -> None:
+    """
+    stampa il grafico dei valori che si trovano nella struttura dati globale algorithms
+
+    Args:
+        x_label: come nominare l'asse delle x del grafico
+        log_scale: si può scegliere scalare gli assi
+    """
+    for idx, algo_config in enumerate(algorithms, start=1):
+        # plot del grafico misurato
+        plt.plot(algo_config["ascisse"], algo_config["ordinate"], marker='o', linestyle='', color=algo_config["color"], label=algo_config["name"])
+
+        # plot del grafico teorico
+        log_x = np.log10(algo_config["ascisse"])
+        log_y = np.log10(algo_config["ordinate"])
+        coeff = np.polyfit(log_x, log_y, 3)  # Fitting polinomiale di grado 3
+        fitted_curve = np.poly1d(coeff)
+        fitted_y = 10**fitted_curve(log_x) # Generazione dei valori della curva di adattamento
+        plt.plot(algo_config["ascisse"], fitted_y, linestyle='-', color=(algo_config["color"] + "20"))
+    plt.xlabel(x_label)
+    plt.ylabel('t(' + x_label + ')')
+    plt.legend()
+    plt.tight_layout()
+    if log_scale:
+        plt.xscale("log")
+        plt.yscale("log")
+    plt.show()
+
+def main():
+    misuraTempiSullaBaseDi('n', var_start=N_MIN, var_end=N_MAX, lock=M_LOCK)
+    stampaGraficoDeiValoriMisurati('n')
+
+    misuraTempiSullaBaseDi('m', var_start=M_MIN, var_end=M_MAX, lock=N_LOCK)
+    stampaGraficoDeiValoriMisurati('m')
+
+
+
+
+main()
